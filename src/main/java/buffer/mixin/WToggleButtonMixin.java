6a3b4d0255e802cfebea99dc3e25021ed122c273
Ramid Khan <ramidzkh@gmail.com>
2019-09-29 13:47:00 +1000

    Update WToggleButtonMixin to not use @Overwrite

--- src/main/java/buffer/mixin/WToggleButtonMixin.java
+++ src/main/java/buffer/mixin/WToggleButtonMixin.java
@@ -1,48 +1,29 @@
 package buffer.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-
-import io.github.cottonmc.cotton.gui.client.LibGuiClient;
-import io.github.cottonmc.cotton.gui.client.ScreenDrawing;
 import io.github.cottonmc.cotton.gui.widget.WToggleButton;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 // LibGUI, none of this would be necessary if you backported.
-
-@Mixin(WToggleButton.class)
+@Mixin(value = WToggleButton.class, remap = false)
 public class WToggleButtonMixin {
-    @Shadow(remap=false)
-    boolean isOn;
 
-    @Shadow(remap=false)
-    Identifier onImage;
-    
-    @Shadow(remap=false)
-    Identifier offImage;
-    
-    @Shadow(remap=false)
-    Text label;
+	@Shadow
+	protected Identifier onImage;
 
-    @Shadow(remap=false)
-    int color;
-    
-    @Shadow(remap=false)
-	int darkmodeColor;
+	@Shadow
+	protected Identifier offImage;
 
-    @Overwrite(remap=false)
-    @Environment(EnvType.CLIENT)
-    public void paintBackground(int x, int y) {
-		ScreenDrawing.rect(isOn ? onImage : offImage, x, y, 18, 18, 0xFFFFFFFF);
-		
-		if (label!=null) {
-
-			ScreenDrawing.drawString(label.asFormattedString(), x + 22, y+6, LibGuiClient.config.darkMode ? darkmodeColor : color);
-        }
+	@Redirect(method = "paintBackground", at = @At(value = "FIELD", target = "Lio/github/cottonmc/cotton/gui/widget/WToggleButton;DEFAULT_ON_IMAGE:Lnet/minecraft/util/Identifier;"))
+	private Identifier getOnImage() {
+		return onImage;
 	}
-    
+
+	@Redirect(method = "paintBackground", at = @At(value = "FIELD", target = "Lio/github/cottonmc/cotton/gui/widget/WToggleButton;DEFAULT_OFF_IMAGE:Lnet/minecraft/util/Identifier;"))
+	private Identifier getOffImage() {
+		return offImage;
+	}
 }
