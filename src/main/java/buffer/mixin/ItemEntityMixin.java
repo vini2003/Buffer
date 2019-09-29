d0bb5768c64ef06cd35b3962d4c3d3e3ac8a71d4
Ramid Khan <ramidzkh@gmail.com>
2019-09-29 14:29:11 +1000

    Fix ItemEntityMixin
    It wouldn't work for dedicated servers before

--- src/main/java/buffer/mixin/ItemEntityMixin.java
+++ src/main/java/buffer/mixin/ItemEntityMixin.java
@@ -1,13 +1,5 @@
 package buffer.mixin;
 
-import java.util.UUID;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import buffer.inventory.BufferInventory;
 import buffer.registry.ItemRegistry;
 import buffer.screen.BufferItemController;
@@ -16,39 +8,47 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.stat.Stats;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin {
-    @Shadow
-    UUID owner;
 
-    @Shadow
-    int age;
+	@Shadow
+	private UUID owner;
 
-    @Shadow
-    int pickupDelay;
+	@Shadow
+	private int age;
 
-    @Inject(method = "onPlayerCollision(Lnet/minecraft/entity/player/PlayerEntity;)V", at = @At("HEAD"))
-    protected void onPlayerCollision(PlayerEntity playerEntity, CallbackInfo info) {
-        ItemEntity itemEntity = (ItemEntity)(Object)this;
-        ItemStack buffer = ItemStack.EMPTY;
-        for (ItemStack stack : playerEntity.inventory.main) {
-            if (stack.getItem() == ItemRegistry.BUFFER_ITEM) {
-                buffer = stack;
-            }
-        }
-        if (!itemEntity.world.isClient && buffer != ItemStack.EMPTY && !(playerEntity.container instanceof BufferItemController) && buffer.getTag().getBoolean("pickup") == true) {
-            ItemStack itemStack = itemEntity.getStack();
-            if (itemStack.getItem() != ItemRegistry.BUFFER_ITEM && pickupDelay == 0 && (owner == null || 6000 - age <= 200 || owner.equals(playerEntity.getUuid()))) {
-                BufferInventory bufferInventory = BufferInventory.fromTag(buffer.getTag());
-                ItemStack tryInsertStack = bufferInventory.insertStack(itemStack);
-                itemEntity.setStack(tryInsertStack);
-                buffer.setTag(BufferInventory.toTag(bufferInventory, new CompoundTag()));
-                playerEntity.sendPickup(itemEntity, itemStack.getCount());
-                playerEntity.increaseStat(Stats.PICKED_UP.getOrCreateStat(itemStack.getItem()), itemStack.getCount());
-            } else {
-                return;
-            }
-        }
-    }
+	@Shadow
+	private int pickupDelay;
+
+	@Inject(method = "onPlayerCollision", at = @At("HEAD"))
+	private void onPlayerCollision(PlayerEntity playerEntity, CallbackInfo info) {
+		ItemEntity itemEntity = (ItemEntity) (Object) this;
+		ItemStack buffer = ItemStack.EMPTY;
+
+		for (ItemStack stack : playerEntity.inventory.main) {
+			if (stack.getItem() == ItemRegistry.BUFFER_ITEM) {
+				buffer = stack;
+			}
+		}
+
+		if (!itemEntity.world.isClient && buffer != ItemStack.EMPTY && !(playerEntity.container instanceof BufferItemController) && buffer.getTag().getBoolean("pickup") == true) {
+			ItemStack itemStack = itemEntity.getStack();
+			if (itemStack.getItem() != ItemRegistry.BUFFER_ITEM && pickupDelay == 0 && (owner == null || 6000 - age <= 200 || owner.equals(playerEntity.getUuid()))) {
+				BufferInventory bufferInventory = BufferInventory.fromTag(buffer.getTag());
+				ItemStack tryInsertStack = bufferInventory.insertStack(itemStack);
+				itemEntity.setStack(tryInsertStack);
+				buffer.setTag(BufferInventory.toTag(bufferInventory, new CompoundTag()));
+				playerEntity.sendPickup(itemEntity, itemStack.getCount());
+				playerEntity.increaseStat(Stats.PICKED_UP.getOrCreateStat(itemStack.getItem()), itemStack.getCount());
+			}
+		}
+	}
 }
